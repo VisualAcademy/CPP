b999a84049145a33d903f467f31c046dafd15ae3
Park YongJune <redplus@redplus.net>
2021-12-29 18:32:28 +0900

    input-name

---
+++ CPP/InputName.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+    string name;
+
+    cout << "ÀÌ¸§: ";
+
+    cin >> name;
+
+    cout << "¾È³çÇÏ¼¼¿ä? " << name << "´Ô!!!" << endl;
+
+    return 0;
+}
