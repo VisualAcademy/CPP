893eca438273a6fcb929f475f58fe2a15046c70f
Park YongJune <redplus@redplus.net>
2022-01-13 13:37:22 +0900

    .

---
+++ CPP/TypeInference.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <typeinfo>
+
+using namespace std;
+
+int main(void)
+{
+    auto num1 = 1234;
+    auto num2 = 3.14;
+
+    cout << num1 << endl;
+    cout << num2 << endl;
+
+    return 0;
+}
